fdbca87194fa6352b78b0525b572635b71008160
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-07-09 12:01:15 +0200

    First commit: initialisation of Sprint boot project

---
+++ backend-mystery-word/src/main/java/com/portfolio/backend_mystery_word/BackendMysteryWordApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.backend_mystery_word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMysteryWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMysteryWordApplication.class, args);
+	}
+
+}
---
+++ backend-mystery-word/src/test/java/com/portfolio/backend_mystery_word/BackendMysteryWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.backend_mystery_word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMysteryWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
